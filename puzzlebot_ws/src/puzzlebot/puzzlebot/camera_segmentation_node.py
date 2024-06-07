922c2ea4ef54f77bdb8210f23f8a3a4e80924c97
PaoloReyes <paolo.alfonso.reyes@gmail.com>
2024-06-06 22:57:00 -0600

    camera node

--- puzzlebot_ws/src/puzzlebot/puzzlebot/camera_segmentation_node.py
+++ puzzlebot_ws/src/puzzlebot/puzzlebot/camera_segmentation_node.py
@@ -28,15 +28,7 @@ class CameraNode(Node):
         else:
             print('Unable to open camera')
     
-    def gstreamer_pipeline(
-        sensor_id=0,
-        capture_width=320,
-        capture_height=240,
-        display_width=320,
-        display_height=240,
-        framerate=30,
-        flip_method=0,
-    ):
+    def gstreamer_pipeline(self, sensor_id=0, capture_width=320, capture_height=240, display_width=320, display_height=240,framerate=30, flip_method=0):
         return (
             "nvarguscamerasrc sensor-id=%d ! "
             "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
@@ -44,15 +36,7 @@ class CameraNode(Node):
             "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
             "videoconvert ! "
             "video/x-raw, format=(string)BGR ! appsink"
-            % (
-                sensor_id,
-                capture_width,
-                capture_height,
-                framerate,
-                flip_method,
-                display_width,
-                display_height,
-            )
+            % (sensor_id, capture_width, capture_height, framerate, flip_method, display_width, display_height)
         )
                 
 def main(args=None):
