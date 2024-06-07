735da189d04cde4d6800564949ef8cb9232ad186
RikuNav <ricardonavarro2003@gmail.com>
2024-06-07 10:33:16 -0600

    calibration methods

---
+++ utilities/get_calib_image.py
@@ -0,0 +1,23 @@
+import cv2
+from camera_demo import gstreamer_pipeline
+
+cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+num = 0
+
+while cap.isOpened():
+
+    succes, img = cap.read()
+    k = cv2.waitKey(5)
+    if k == 27:
+        break
+    elif k == ord('z'): # wait for 'z' key to save and exit
+        cv2.imwrite('images/img' + str(num) + '.png', img)
+        print("image saved!")
+        num += 1
+
+    cv2.imshow('Img',img)
+
+# Release and destroy all windows before termination
+cap.release()
+
+cv2.destroyAllWindows()
