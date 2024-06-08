e8580f5f1b76684c55ffeff598f76e8395944426
PaoloReyes <paolo.alfonso.reyes@gmail.com>
2024-06-08 16:36:30 -0600

    trying to quatize the model

--- puzzlebot_ws/src/puzzlebot/puzzlebot/camera_segmentation_node.py
+++ puzzlebot_ws/src/puzzlebot/puzzlebot/camera_segmentation_node.py
@@ -56,7 +56,7 @@ class CameraNode(Node):
             msg = String()
             mask_raw = result.masks[0].cpu().data.numpy().transpose(1, 2, 0)
             mask_3channel = cv2.merge((mask_raw, mask_raw, mask_raw))
-            cv2.imshow('YOLOv8 Inference', mask_3channel)
+            cv2.imshow('YOLOv', mask_3channel)
 
             cv2.imshow('YOLOv8 Inference', image)
             cv2.waitKey(1)
