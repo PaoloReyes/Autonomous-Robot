639e77cd16689c573908b61ba1c524a74968cf69
RikuNav <ricardonavarro2003@gmail.com>
2024-06-08 22:24:25 -0600

    merge lines

--- puzzlebot_ws/src/puzzlebot/puzzlebot/camera_segmentation_node.py
+++ puzzlebot_ws/src/puzzlebot/puzzlebot/camera_segmentation_node.py
@@ -86,18 +86,20 @@ class CameraNode(Node):
                         cv2.line(dst, (x1, y1), (x2, y2), (255, 0, 0), 2)
                         group[2].append((x1,y1, x2, y2))
 
-                for i in range(len(group)):
-                    coord_group = [[], [], []]
-                    if len(group[i]) > 1:
-                        for k in range(len(group[i])):
-                            coord_group[i] = self.merge_two_lines(group[i][k], coord_group[i])
-                    else:
-                        coord_group[i] = group[i]
+                print(group)
 
-                for i in range(len(coord_group)):
-                    if len(coord_group[i]) > 0:
-                        x1, y1, x2, y2 = coord_group[i]
-                        cv2.line(dst, (x1, y1), (x2, y2), (15, 219, 133), 2)
+                # for i in range(len(group)):
+                #     coord_group = [[], [], []]
+                #     if len(group[i]) > 1:
+                #         for k in range(len(group[i])):
+                #             coord_group[i] = self.merge_two_lines(group[i][k], coord_group[i])
+                #     else:
+                #         coord_group[i] = group[i]
+
+                # for i in range(len(coord_group)):
+                #     if len(coord_group[i]) > 0:
+                #         x1, y1, x2, y2 = coord_group[i]
+                #         cv2.line(dst, (x1, y1), (x2, y2), (15, 219, 133), 2)
 
             cv2.imshow('Original Image', dst)
             cv2.imshow('edges', edges)
