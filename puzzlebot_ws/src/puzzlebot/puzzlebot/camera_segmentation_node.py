bb2bd738a930914147ee327cdcb204048e02f8c8
PaoloReyes <paolo.alfonso.reyes@gmail.com>
2024-06-09 13:38:02 -0600

    start lines

--- puzzlebot_ws/src/puzzlebot/puzzlebot/camera_segmentation_node.py
+++ puzzlebot_ws/src/puzzlebot/puzzlebot/camera_segmentation_node.py
@@ -65,8 +65,28 @@ class CameraNode(Node):
             edges = cv2.Canny(blurred_mask, 100, 200)
 
             # search the start point of each line of street
+            start_point = None
+            end_point = None
             for y in range(edges.shape[0]-1, -1, -1):
                 print(y)
+                for x in range(edges.shape[1]):
+                    if edges[y, x] == 255:
+                        start_point = (x, y)
+                        break
+                if start_point is not None:
+                    for x in range(edges.shape[1]-1, -1, -1):
+                        if edges[y, x] == 255:
+                            end_point = (x, y)
+                            break
+                if start_point is not None and end_point is not None:
+                    if np.abs(start_point[0] - end_point[0]) > 50:
+                        break
+                    else:
+                        start_point = None
+                        end_point = None
+            
+            if start_point is not None and end_point is not None:
+                print(start_point, end_point)
                 # start_point_1 = None
                 # start_point_2 = None
                 # for x in range(edges.shape[1]):
