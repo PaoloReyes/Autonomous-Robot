32dc8d6962441e60691a9fba263017f2336f7ede
PaoloReyes <paolo.alfonso.reyes@gmail.com>
2024-06-09 06:08:58 -0600

    joining lines

--- puzzlebot_ws/src/puzzlebot/puzzlebot/camera_segmentation_node.py
+++ puzzlebot_ws/src/puzzlebot/puzzlebot/camera_segmentation_node.py
@@ -97,7 +97,7 @@ class CameraNode(Node):
 
             for i, group in enumerate(groups):
                 for line in group:
-                    line[1] = group_max[i]
+                    line = (line[0], group_max[i], line[2], line[3])
 
             merged_lines = []
             for i, group in enumerate(groups):
