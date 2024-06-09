cff99fb3465299747b36a5db6385b2fce560adcd
RikuNav <ricardonavarro2003@gmail.com>
2024-06-09 17:06:47 -0600

    final

--- puzzlebot_ws/src/puzzlebot/puzzlebot/camera_segmentation_node.py
+++ puzzlebot_ws/src/puzzlebot/puzzlebot/camera_segmentation_node.py
@@ -70,7 +70,7 @@ class CameraNode(Node):
 
             mid_images = edges.copy()
             cv2.line(mid_images, (0, mid_images.shape[0]//2), (mid_images.shape[1], mid_images.shape[0]//2), (255, 255, 255), 1)
-            mid = img_masked[mid_images.shape[0]//2:,:]
+            mid = blurred_mask[blurred_mask.shape[0]//2:,:]
             
             [contours, _] = cv2.findContours(mid, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
             if len(contours) > 0:
