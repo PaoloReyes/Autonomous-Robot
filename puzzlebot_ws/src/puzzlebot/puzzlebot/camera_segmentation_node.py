de8596aae26059fbc84f4108dfb820f5b40463e7
PaoloReyes <paolo.alfonso.reyes@gmail.com>
2024-06-09 05:35:47 -0600

    joining lines

--- puzzlebot_ws/src/puzzlebot/puzzlebot/camera_segmentation_node.py
+++ puzzlebot_ws/src/puzzlebot/puzzlebot/camera_segmentation_node.py
@@ -85,7 +85,7 @@ class CameraNode(Node):
                     else:
                         groups[2].append((x1, y1, x2, y2))
 
-            merged_lines = [[], [], []]
+            merged_lines = []
             for i, group in enumerate(groups):
                 if len(group) > 0:
                     merged_line = group[0]
@@ -95,6 +95,8 @@ class CameraNode(Node):
                         merged_lines[i].append(merged_line)
                     else:
                         merged_lines[i].append(group[0])
+                else:
+                    merged_lines[i].append(None)
             
             print()
             print(groups)
@@ -102,6 +104,7 @@ class CameraNode(Node):
             print()
             
             for i, line in enumerate(merged_lines):
+                if line is None: continue
                 x1, y1, x2, y2 = line
                 if i == 0:
                     cv2.line(dst, (x1, y1), (x2, y2), (241, 111, 188), 2)
