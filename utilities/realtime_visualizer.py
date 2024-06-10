ef1aa7f2314ee43de6811db4a2d5e22492ff044e
RikuNav <ricardonavarro2003@gmail.com>
2024-06-09 22:09:09 -0600

    new model

--- utilities/realtime_visualizer.py
+++ utilities/realtime_visualizer.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from ultralytics import YOLO
 
 def main():
-    MODEL_PATH = r'D:\Tec\Sexto_Semestre\Autonomous-Robot\yolov8train\runs\segment\train20\weights\best.pt'
+    MODEL_PATH = r'D:\rokur\Documents\ITESM\6to\Autonomous-Robot\yolov8train\runs\segment\train21\weights\best.pt'
     
     model = YOLO(MODEL_PATH)
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
