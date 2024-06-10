20145e69e1f9c95e73c3899825c5fc36e6bdeb83
RikuNav <ricardonavarro2003@gmail.com>
2024-06-09 18:06:57 -0600

    final

--- puzzlebot_ws/src/puzzlebot/puzzlebot/camera_segmentation_node.py
+++ puzzlebot_ws/src/puzzlebot/puzzlebot/camera_segmentation_node.py
@@ -80,7 +80,6 @@ class CameraNode(Node):
                     cv2.circle(edges, (cx, cy), 5, (255, 255, 255), -1)
     
             mid_edge = edges[edges.shape[0]//2, :]
-            mid_edge = cv2.resize(mid_edge, (320, 240))
 
             cv2.imshow('Original Image', img)
             cv2.imshow('edges', edges)
