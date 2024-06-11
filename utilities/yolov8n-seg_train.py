2959f02c13204d9346f8ed3e2b8886a248b5a24a
PaoloReyes <paolo.alfonso.reyes@gmail.com>
2024-06-11 03:21:16 -0600

    10 epoch model finaaaal

--- utilities/yolov8n-seg_train.py
+++ utilities/yolov8n-seg_train.py
@@ -2,5 +2,5 @@ from roboflow import Roboflow
 
 rf = Roboflow(api_key="WVwS1qsuBAjRcIrfsYmi")
 project = rf.workspace("ilovecantoral").project("semanticsegmentation-lamyy")
-version = project.version(11)
+version = project.version(12)
 dataset = version.download("coco-segmentation")
--- yolov8train/train_yolo.bat
+++ yolov8train/train_yolo.bat
@@ -1,2 +1,2 @@
 call conda activate ai
-yolo task=segment mode=train model=yolov8n-seg.pt data=semanticsegmentation-11_yolo\data.yaml batch=16 epochs=100 imgsz=320 close_mosaic=25 fliplr=0.0 scale=0.0 iou=0.4
+yolo task=segment mode=train model=yolov8n-seg.pt data=semanticsegmentation-12_yolo\data.yaml batch=16 epochs=100 imgsz=320 close_mosaic=25 fliplr=0.0 scale=0.0 iou=0.4
