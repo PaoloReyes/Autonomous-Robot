cdf574b96d713c3849926ea119a4e6ef44b403b9
PaoloReyes <paolo.alfonso.reyes@gmail.com>
2024-06-11 00:17:19 -0600

    10 epochs model

--- utilities/yolov8n-seg_train.py
+++ utilities/yolov8n-seg_train.py
@@ -2,5 +2,5 @@ from roboflow import Roboflow
 
 rf = Roboflow(api_key="WVwS1qsuBAjRcIrfsYmi")
 project = rf.workspace("ilovecantoral").project("semanticsegmentation-lamyy")
-version = project.version(10)
+version = project.version(11)
 dataset = version.download("coco-segmentation")
