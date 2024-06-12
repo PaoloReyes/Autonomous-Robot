03a63be055da74e37c65c4ddd7f33be0c6525128
PaoloReyes <paolo.alfonso.reyes@gmail.com>
2024-06-11 20:34:16 -0600

    better launch

--- puzzlebot_ws/src/puzzlebot/launch/puzzlebot_launch.py
+++ puzzlebot_ws/src/puzzlebot/launch/puzzlebot_launch.py
@@ -1,14 +1,13 @@
 import os
 from launch import LaunchDescription
 from ament_index_python import get_package_share_directory
-from launch.actions import IncludeLaunchDescription
 from launch_ros.actions import Node
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import ExecuteProcess
 
 def generate_launch_description() -> LaunchDescription:
 
-    manchester_camera_launch_path = os.path.join(get_package_share_directory('ros_deep_learning'), 'launch', 'video_source.ros2.launch')
-    
+    manchester_camera_launch = ExecuteProcess(cmd=['ros2', 'launch', 'ros_deep_learning', 'video_source.ros2.launch.py'], output='screen')
+
     micro_ros_Node = Node(
         package='micro_ros_agent',
         executable='micro_ros_agent',
@@ -17,8 +16,6 @@ def generate_launch_description() -> LaunchDescription:
     )
 
     return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(manchester_camera_launch_path),
-        ),
+        manchester_camera_launch,
         micro_ros_Node
     ])
