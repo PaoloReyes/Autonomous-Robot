1e9d9c719d2f69948257f34b70c85d4119b7c1d8
PaoloReyes <paolo.alfonso.reyes@gmail.com>
2024-06-11 20:36:20 -0600

    better launch

--- puzzlebot_ws/src/puzzlebot/launch/puzzlebot_launch.py
+++ puzzlebot_ws/src/puzzlebot/launch/puzzlebot_launch.py
@@ -6,7 +6,7 @@ from launch.actions import ExecuteProcess
 
 def generate_launch_description() -> LaunchDescription:
 
-    manchester_camera_launch = ExecuteProcess(cmd=['ros2', 'launch', 'ros_deep_learning', 'video_source.ros2.launch.py'], output='screen')
+    manchester_camera_launch = ExecuteProcess(cmd=['ros2', 'launch', 'ros_deep_learning', 'video_source.ros2.launch'], output='screen')
 
     micro_ros_Node = Node(
         package='micro_ros_agent',
