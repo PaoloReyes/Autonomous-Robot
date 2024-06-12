2426bb2ad26d78b4d6059dbbfd297263c70ad01b
PaoloReyes <paolo.alfonso.reyes@gmail.com>
2024-06-11 20:59:58 -0600

    controller node

--- puzzlebot_ws/src/puzzlebot/launch/puzzlebot_launch.py
+++ puzzlebot_ws/src/puzzlebot/launch/puzzlebot_launch.py
@@ -15,7 +15,15 @@ def generate_launch_description() -> LaunchDescription:
         arguments=['serial', '--dev', '/dev/ttyUSB0'],
     )
 
+    controller_node = Node(
+        package='puzzlebot',
+        executable='controller_node',
+        output='screen',
+        parameters=[os.path.join(get_package_share_directory('puzzlebot'), 'config', 'controller_node_params.yaml')]
+    )
+
     return LaunchDescription([
         manchester_camera_launch,
+        controller_node,
         micro_ros_Node
     ])
